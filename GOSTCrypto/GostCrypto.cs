5f83840b4ca2b97ccd5bae602605fcb8ef810cb1
Pavel Ushakov <p.ushakov.2003@gmail.com>
2023-11-17 18:53:45 +0300

    first commit

---
+++ GOSTCrypto/GostCrypto.cs
@@ -0,0 +1,176 @@
+﻿using System.Text;
+
+namespace GostCrypto;
+
+public class GostCrypto
+{
+    static void Main()
+    {
+        var encoded = Encode(
+            Encoding.UTF8.GetBytes("12345678123456781234567812345678"),
+            Encoding.UTF8.GetBytes("b14afb2db14afb2db14afb2db14afb2d"));
+        Console.WriteLine(Encoding.UTF8.GetString(encoded));
+
+        var decoded = Decode(encoded, Encoding.UTF8.GetBytes("b14afb2db14afb2db14afb2db14afb2d"));
+        Console.WriteLine(Encoding.UTF8.GetString(decoded));
+        Console.ReadKey();
+    }
+
+    protected static byte[][] SubstitutionBox =
+    {
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF },
+        new byte[] { 0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF } 
+    };
+
+    protected static uint[] GenerateKeys(byte[] key)
+    {
+        if (key.Length != 32)
+        {
+            throw new Exception("Wrong key.");
+        }
+
+        var subkeys = new uint[8];
+            
+        for (int i = 0; i < 8; i++)
+        {
+            subkeys[i] = BitConverter.ToUInt32(key, 4*i);
+        }
+
+        return subkeys;
+    }
+
+    protected static uint Substitution(uint value)
+    {
+        uint output = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            var temp = (byte) ((value >> (4*i)) & 0x0f);
+            temp = SubstitutionBox[i][temp];
+            output |= (UInt32) temp << (4*i);
+        }
+
+        return output;
+    }
+
+    public static byte[] Encode(byte[] data, byte[] key)
+    {
+        var subkeys = GenerateKeys(key);
+        var result = new byte[data.Length];
+        var block = new byte[8];
+        for (int i = 0; i < data.Length / 8; i++)
+        {
+            Array.Copy(data, 8 * i, block, 0 , 8);
+            Array.Copy(EncodeBlock(block, subkeys), 0, result, 8 * i, 8);
+        }
+
+        return result;
+    }
+    
+    private static byte[] EncodeBlock(byte[] block, uint[] keys)
+    {
+        // separate on 2 blocks.
+        uint N1 = BitConverter.ToUInt32(block, 0);
+        uint N2 = BitConverter.ToUInt32(block, 4);
+            
+        for (int i = 0; i < 32; i++)
+        {
+            int keyIndex = i < 24 ? (i%8) : (7 - i%8); // to 24th cycle : 0 to 7; after - 7 to 0;
+            var s = (N1 + keys[keyIndex]) % uint.MaxValue; // (N1 + X[i]) mod 2^32
+            s = Substitution(s); // substitute from box
+            s = (s << 11) | (s >> 21);
+            s = s ^ N2; // ( s + N2 ) mod 2
+            //N2 = N1;
+            //N1 = s;
+            if (i < 31) // last cycle : N1 don't change; N2 = s;
+            {
+                N2 = N1;
+                N1 = s;
+            }
+            else
+            {
+                N2 = s;
+            }
+        }
+
+        var output = new byte[8];
+        var N1buff = BitConverter.GetBytes(N1);
+        var N2buff = BitConverter.GetBytes(N2);
+
+        for (int i = 0; i < 4; i++)
+        {
+            output[i] = N1buff[i];
+            output[4 + i] = N2buff[i];  
+        } 
+
+        return output;
+    }
+    
+    protected static byte[] Decode(byte[] data, byte[] key, bool isParallel = false)
+    {
+        var subkeys = GenerateKeys(key);
+        var result = new byte[data.Length];
+        var block = new byte[8];
+
+        if (isParallel)
+        {
+            Parallel.For(0, data.Length/8, i =>
+            {
+                Array.Copy(data, 8 * i, block, 0, 8);
+                Array.Copy(DecodeBlock(block, subkeys), 0, result, 8 * i, 8);
+            });
+        }
+        else
+        {
+            for (int i = 0; i < data.Length/8; i++) // N blocks 64bits length.
+            {
+                Array.Copy(data, 8*i, block, 0, 8);
+                Array.Copy(DecodeBlock(block, subkeys), 0, result, 8*i, 8);
+            }
+        }
+        return result;
+    }
+
+    private static byte[] DecodeBlock(byte[] block, uint[] keys)
+    {
+        // separate on 2 blocks.
+        uint N1 = BitConverter.ToUInt32(block, 0);
+        uint N2 = BitConverter.ToUInt32(block, 4);
+        
+        for (int i = 0; i < 32; i++)
+        {
+            int keyIndex = i < 8 ? (i%8) : (7 - i%8); // to 24th cycle : 0 to 7; after - 7 to 0;
+            var s = (N1 + keys[keyIndex])%uint.MaxValue; // (N1 + X[i]) mod 2^32
+            s = Substitution(s); // substitute from box
+            s = (s << 11) | (s >> 21);
+            s = s ^ N2; 
+            if (i < 31) // last cycle : N1 don't change; N2 = s;
+            {
+                N2 = N1;
+                N1 = s;
+            }
+            else
+            {
+                N2 = s;
+            }
+        }
+
+        var output = new byte[8];
+        var N1buff = BitConverter.GetBytes(N1);
+        var N2buff = BitConverter.GetBytes(N2);
+
+        for (int i = 0; i < 4; i++)
+        {
+            output[i] = N1buff[i]; 
+            output[4 + i] = N2buff[i];
+        }
+
+        return output;
+    }
+}
